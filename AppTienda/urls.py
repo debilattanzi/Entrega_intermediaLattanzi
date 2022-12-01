dc07f2b7b11faf2e144928fc8d7ef7723c9e6e07
Debi Lattanzi <deboralattanzi@hotmail.com>
2022-12-01 19:48:49 -0300

    agregue nuevos templates

--- AppTienda/urls.py
+++ AppTienda/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from AppTienda.views import *
 
 urlpatterns = [
-    path("", inicio, name="inicio")
+    path("", inicio, name="inicio"),
+    path('proveedor/', proveedor, name="proveedor"),
+    path('prendas/', prendas, name="prendas")
 ]
--- AppTienda/views.py
+++ AppTienda/views.py
@@ -6,3 +6,9 @@ def inicio(request):
     return render(request, "inicio.html")
 
 
+def proveedor(request):
+    return render(request, "proveedor.html")
+
+
+def prendas(request):
+    return render(request, "prendas.html")
