83aaf505311f2a040d7ac961f4239d0117b45066
Debi Lattanzi <deboralattanzi@hotmail.com>
2022-12-06 23:02:49 -0300

    correccion error pagina de inicio, agregue path inicio en urls del proyecto

--- Entrega_Intermedia_Lattanzi/urls.py
+++ Entrega_Intermedia_Lattanzi/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from AppTienda.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppTienda/', include('AppTienda.urls'))
+    path('AppTienda/', include('AppTienda.urls')),
+    path("", inicio, name="inicio")
+
 ]
